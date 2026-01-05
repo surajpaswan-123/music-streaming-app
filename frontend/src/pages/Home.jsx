c707d1176db696d67abbcd0169dd12b4cd4aeff0
suraj paswan <sp437107@gmail.com>
2026-01-06 00:34:40 +0530

    feat(frontend): create Home page component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="page">
+      <h2>Home</h2>
+      <p>Welcome to the Music Streaming App</p>
+      <div className="placeholder">
+        <p>🎵 Featured playlists will appear here</p>
+        <p>🎧 Recently played tracks will appear here</p>
+        <p>⭐ Recommended songs will appear here</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
