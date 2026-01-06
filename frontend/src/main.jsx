65f1ec5484d25398486401295810706ddbbcc744
suraj paswan <sp437107@gmail.com>
2026-01-06 23:57:42 +0530

    feat: register service worker in main.jsx for PWA support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
+import { registerServiceWorker } from './utils/pwa.js'
+
+// Register service worker for PWA
+registerServiceWorker();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
